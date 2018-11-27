527536a1608f633d0882dfd404955c92423c866b
sserges <agounkeserge@gmail.com>
2018-11-27 15:28:25 +0000

    addItemWindow added

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const path = require('path');
 const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
+let addWindow;
 
 // Listen for app to be ready
 app.on('ready', function() {
@@ -23,18 +24,40 @@ app.on('ready', function() {
 	Menu.setApplicationMenu(mainMenu);
 });
 
+
+// Handle create add window
+function createAddWindow() {
+	// Create new window
+	addWindow = new BrowserWindow({
+		width: 300,
+		height: 200,
+		title: 'Add Shopping List Item'
+	});
+	// Load html into window
+	addWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'addWindow.html'),
+		protocol: 'file',
+		slashes: true
+	}));
+}
+
+// Create menu template
 const mainMenuTemplate = [
 	{
 		label: 'File',
 		submenu:[
 			{
-				label: 'Add Item'
+				label: 'Add Item',
+				click() {
+					createAddWindow();
+				}
 			},
 			{
 				label: 'Clear Items',
 			},
 			{
 				label: 'Quit',
+				accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
 				click() {
 					app.quit();
 				}
