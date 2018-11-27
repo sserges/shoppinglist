516f08fc576af8801cf70f3d8630875ff9933b73
sserges <agounkeserge@gmail.com>
2018-11-27 14:47:21 +0000

    package.json added

---
+++ main.js
@@ -0,0 +1,44 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu} = electron;
+
+let mainWindow;
+
+// Listen for app to be ready
+app.on('ready', function() {
+	// Create new window
+	mainWindow = new BrowserWindow({});
+	// Load html into window
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'mainWindow.html'),
+		protocol: 'file',
+		slashes: true
+	}));
+
+	// Build menu from template
+	const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+	// Insert Menu
+	Menu.setApplicationMenu(mainMenu);
+});
+
+const mainMenuTemplate = [
+	{
+		label: 'File',
+		submenu:[
+			{
+				label: 'Add Item'
+			},
+			{
+				label: 'Clear Items',
+			},
+			{
+				label: 'Quit',
+				click() {
+					app.quit();
+				}
+			}
+		]
+	}
+];
